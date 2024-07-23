a2bbb2a930fee2657cf1999cd866794f4b410721
Bhawna0034 <bhawnaasharma2002@gmail.com>
2024-07-24 00:22:43 +0530

    lecture 3.3

--- src/main/java/com/bhawna/SpringBootDataJPA/DataJPA/entities/ProductEntity.java
+++ src/main/java/com/bhawna/SpringBootDataJPA/DataJPA/entities/ProductEntity.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.cglib.core.Local;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -29,7 +28,7 @@ import java.time.LocalDateTime;
         }
 )
 
-public class Product {
+public class ProductEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/bhawna/SpringBootDataJPA/DataJPA/DataJpaApplicationTests.java
+++ src/test/java/com/bhawna/SpringBootDataJPA/DataJPA/DataJpaApplicationTests.java
@@ -1,13 +1,55 @@
 package com.bhawna.SpringBootDataJPA.DataJPA;
 
+import com.bhawna.SpringBootDataJPA.DataJPA.entities.ProductEntity;
+import com.bhawna.SpringBootDataJPA.DataJPA.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class DataJpaApplicationTests {
+
+	@Autowired
+	ProductRepository productRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
-}
+	@Test
+	void testRepository(){
+		ProductEntity productEntity = ProductEntity.builder()
+				.price(BigDecimal.valueOf(9.5))
+				.quantity(2)
+				.sku("nestle799")
+				.title("Nestle Chocolate")
+				.build();
+
+		ProductEntity savedProductEntity = productRepository.save(productEntity);
+		System.out.println(productEntity);
+	}
+
+	@Test
+	void getRepository(){
+		//List<ProductEntity> productEntities = productRepository.findByQuantityAndPrice(2,BigDecimal.valueOf(9.5));
+	    //List<ProductEntity> productEntities = productRepository.findByTitleLike("%Choco%");
+		List<ProductEntity> productEntities = productRepository.findByTitleContaining("Choco");
+			System.out.println(productEntities);
+		}
+
+	@Test
+	void getSingleFromRepository(){
+		Optional<ProductEntity> entity = productRepository.findByTitleAndPrice("Coca-cola",BigDecimal.valueOf(18.5));
+		entity.ifPresent(System.out::println);
+	}
+	}
+
+
+
+
+
