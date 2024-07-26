156ae19ec4cdfe6ebd9dfd3abdf4d5ec034402d5
Bhawna0034 <bhawnaasharma2002@gmail.com>
2024-07-26 18:49:24 +0530

    lecture 3.4

--- src/main/java/com/bhawna/SpringBootDataJPA/DataJPA/repositories/ProductRepository.java
+++ src/main/java/com/bhawna/SpringBootDataJPA/DataJPA/repositories/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.bhawna.SpringBootDataJPA.DataJPA.repositories;
 
 import com.bhawna.SpringBootDataJPA.DataJPA.entities.ProductEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,11 +15,13 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
+    List<ProductEntity> findBy(Sort sort);
+    List<ProductEntity> findByOrderByTitle();
     List<ProductEntity> findByTitle(String pepsi);
     List<ProductEntity> findByCreatedAtAfter(LocalDateTime after);
     List<ProductEntity> findByQuantityAndPrice(Integer quantity, BigDecimal price);
     List<ProductEntity> findByTitleLike(String title);
-    List<ProductEntity> findByTitleContaining(String title);
+    List<ProductEntity> findByTitleContaining(String title, Pageable pageable);
 //    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
 
     @Query("select p from ProductEntity p where p.title=?1 and p.price=?2")
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,7 @@ VALUES
 (12.4, 4, 'parle123', 'Parle Biscuit'),
 (15.6, 1, 'pepsi456', 'Pepsi'),
 (18.5, 3, 'cocacola222', 'Coca-cola'),
-(19.6, 2, 'mazza789', 'Mazza');
+(19.6, 2, 'mazza789', 'Mazza'),
+(14.3, 12, 'munch566', 'Munch'),
+(18.7, 10, 'kitkat304', 'Kit-Kat'),
+(78.3, 2, 'dairymilk204', 'Dairy Milk Silk');
